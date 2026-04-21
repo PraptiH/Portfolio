d036f4f5581f97c1be0d78b084896f521f37bdb5
PraptiH <praptiantara1892@gmail.com>
2026-04-21 19:26:26 +0600

    Fix the navbar at top and change the navbar background color when scroll

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const navbar = document.getElementById('navbar')
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>10){
+        navbar.classList.add('navColor')
+    }
+    else{
+        navbar.classList.remove('navColor')
+    }
+})
+
 const hiddenSection = document.querySelectorAll('.hiddenSection')
 const btn = document.querySelectorAll('.btn')
 btn.forEach(b => {
